498a67c26d5951e00cd6ae3c81ba15388180846f
devsky000 <devsky0@gmail.com>
2026-01-30 10:46:13 +0700

    Setup deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dy_23/',
   plugins: [
     react({
       babel: {
